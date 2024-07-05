4ea55b69a28a92d8c10f6f7057508c210f37b35a
nabilfikrisp <mnabilfikrisp@gmail.com>
2024-07-06 01:03:17 +0700

    deploy: vercel deployment

--- src/components/alert-destructive.tsx
+++
@@ -1,9 +0,0 @@
-import { AlertCircle } from "lucide-react";
-
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-
-export function AlertDestructive() {
-  return (
-    
-  );
-}
